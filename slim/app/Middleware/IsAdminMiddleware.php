e0147a23191e5c036fad3225bdbfba389757cc49
Tomas Sabella <tomassabella@hotmail.com>
2025-09-30 22:11:55 -0300

    Rename AdminMiddleware class to IsAdminMiddleware for consistency

--- slim/app/Middleware/IsAdminMiddleware.php
+++ slim/app/Middleware/IsAdminMiddleware.php
@@ -6,10 +6,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class AdminMiddleware {
+class IsAdminMiddleware {
     
     public function __invoke(Request $request, RequestHandler $handler): Response {
-
+ 
         $isAdmin = $request->getAttribute('is_admin');
         
         if ($isAdmin === null) {
